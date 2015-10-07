9d0493da6417b6863dd6a9083ddef61f7b8ad7d1
Yang Zhou <gavinzhou10c@gmail.com>
2015-10-07 14:10:59 -0400

    [Add] Add getter and setter to header

--- Lab_1/src/Header.java
+++ Lab_1/src/Header.java
@@ -9,7 +9,7 @@ public class Header {
 	private short id;
 	private Header.QR qr;
 	private Header.OPCODE opcode;
-	private boolean AA;
+	private boolean aa;
 	/**
 	 * is Truncated
 	 */
@@ -26,6 +26,86 @@ public class Header {
 	private short nscount;
 	private short arcount;
 	
+	
+	
+	public short getId() {
+		return id;
+	}
+	public void setId(short id) {
+		this.id = id;
+	}
+	public Header.QR getQr() {
+		return qr;
+	}
+	public void setQr(Header.QR qr) {
+		this.qr = qr;
+	}
+	public Header.OPCODE getOpcode() {
+		return opcode;
+	}
+	public void setOpcode(Header.OPCODE opcode) {
+		this.opcode = opcode;
+	}
+	public boolean isAA() {
+		return aa;
+	}
+	public void setAA(boolean aA) {
+		aa = aA;
+	}
+	public boolean isTC() {
+		return TC;
+	}
+	public void setTC(boolean tC) {
+		TC = tC;
+	}
+	public boolean isRD() {
+		return RD;
+	}
+	public void setRD(boolean rD) {
+		RD = rD;
+	}
+	public boolean isRA() {
+		return RA;
+	}
+	public void setRA(boolean rA) {
+		RA = rA;
+	}
+	public short getZ() {
+		return Z;
+	}
+	public void setZ(short z) {
+		Z = z;
+	}
+	public Header.RCODE getRcode() {
+		return rcode;
+	}
+	public void setRcode(Header.RCODE rcode) {
+		this.rcode = rcode;
+	}
+	public short getQdcount() {
+		return qdcount;
+	}
+	public void setQdcount(short qdcount) {
+		this.qdcount = qdcount;
+	}
+	public short getAncount() {
+		return ancount;
+	}
+	public void setAncount(short ancount) {
+		this.ancount = ancount;
+	}
+	public short getNscount() {
+		return nscount;
+	}
+	public void setNscount(short nscount) {
+		this.nscount = nscount;
+	}
+	public short getArcount() {
+		return arcount;
+	}
+	public void setArcount(short arcount) {
+		this.arcount = arcount;
+	}
 	public enum QR {
 		QUERY(0),
 		RESPONSE(1);
